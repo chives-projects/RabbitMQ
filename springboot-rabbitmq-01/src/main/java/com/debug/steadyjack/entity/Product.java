1e888298bc2f63194484324a6e4b27e46dc48a92
achiever-or-loser <1379945771@qq.com>
2019-11-13 21:03:16 +0800

    rabbitMq producer and consumer

---
+++ springboot-rabbitmq-01/src/main/java/com/debug/steadyjack/entity/Product.java
@@ -0,0 +1,67 @@
+package com.debug.steadyjack.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Product implements Serializable {
+    private Integer id;
+
+    private String productNo;
+
+    private Integer total;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProductNo() {
+        return productNo;
+    }
+
+    public void setProductNo(String productNo) {
+        this.productNo = productNo == null ? null : productNo.trim();
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public String toString(){
+        return "Product{" +
+                "id=" + id +
+                ", productNo='" + productNo + '\'' +
+                ", total=" + total +
+                ", createTime=" + createTime +
+                ", updateTime=" + updateTime +
+                '}';
+    }
+}
